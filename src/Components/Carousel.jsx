8f2bc8b672a2fcf919ac996a740a27a13d467b16
ifeanyi azuh <ifeanyiazuh@gmail.com>
2024-12-19 18:51:13 +0600

    k

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { assets } from "../assets/assets"
+
 
 export default function Carousel({ children: slides, autoSlide=false, autoSlideInterval=3500  }) {
   const [curr, setCurr] = useState(0)
